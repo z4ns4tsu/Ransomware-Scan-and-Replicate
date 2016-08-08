c1f7c7b67690c81c2b8dcd32201e478a802c3fec
Jon Turner <jturner@microsoft.com>
2016-08-08 13:25:20 -0700

    PowerShell-ifying and adding native entropy calculation.

--- identify-ransomed-files.ps1
+++ identify-ransomed-files.ps1
@@ -1,46 +1,76 @@
-function checkfile($FNAME) {
-# check file magic char for type - unknown file type has "data" as type
-$cmd = "file -m c:\utils\magic `"" + $FNAME + "`""
-$ftype = iex $cmd
-$copy = 0
-if ($ftype -notlike "*; data*") {$copy = 1 ; return $copy} 
+Param(
+  [Parameter(Mandatory=$false)]
+    [string] $SourceDir = $PWD,
+  [Parameter(Mandatory=$false)]
+    [string] $LogFile = ".\logfile.out"
+)
 
-# check on entropy. If less than 6, it's not encrypted well and is not compressed well, so it's likely data
-$cmd = "sigcheck -a `"" + $FNAME + "`""
-$a = (iex $cmd | select-string -pattern "Entropy:") -split ":"
-#$cmd = "entropy `"" + $FNAME + "`""
-#$a = iex $cmd  
-$entropy = [double]$a[1]
-$copy = 0
-if ($entropy -lt 6) {$copy = 1 }
-return $copy
-}
-
-$src = "c:\vmware"
-$logfile = "logfile.out"
-
-
-iex "cmd /c echo %time% >> t.out"
-
-$files = Get-ChildItem -recurse $src
-$files | foreach {
-  # directories
-  if ($_.psiscontainer) { 
-       # nothing to do here
-       }
-
-  else {
-     $go = checkfile($_.fullname)
-     if ($go -eq 1) { 
-        # nothing to do here
-        # maybe add to the logfile if a -v option is added
+Begin {
+  function getShannonEntropy($file) {
+    $fileEntropy = 0.0
+    $byteCounts = @{}
+    $byteTotal = 0
+    
+    # Folders don't really have entropy, so we'll skip calculating it for them.
+    if(Test-Path $file -PathType Leaf) {
+      $fileBytes = [System.IO.File]::ReadAllBytes($file)
+      
+      foreach ($fileByte in $fileBytes) {
+          $byteCounts[$fileByte]++
+          $byteTotal++
+      }
+  
+      foreach($byte in 0..255) {
+        $byteProb = ([double]$byteCounts[[byte]$byte])/$byteTotal
+        if ($byteProb -gt 0) {
+          $fileEntropy += (-1 * $byteProb) * [Math]::Log($byteProb, 2.0)
         }
-     else { 
-       out-file -filepath $logfile -append -inputobject ($_.fullname + "  - SUSPECTED RANSOMWARE ENCRYPTED")
-       write-host $_.fullname " - SUSPECTED RANSOMWARE ENCRYPTED"
-       }
-     }
+      }
+    }
+    
+    return $fileEntropy
+  }
+  
+  function checkfile($FNAME) {
+    # check file magic char for type - unknown file type has "data" as type
+    $cmd = 'file'
+    $args = @('-m', 'c:\utils\magic', $FNAME)
+    $ftype = & $cmd $args
+    
+    $copy = 0
+    if ($ftype -notmatch ".* data$") {
+      $copy = 1
+    } 
+    else{
+      # check on entropy. If less than 6, it's not encrypted well and is not compressed well, so it's likely data
+      $entropy = getShannonEntropy($FNAME)
+      if ($entropy -lt 6) {$copy = 1 }
+    }
+    
+    Write-Verbose ("{0} - {1}" -f $entropy, $ftype)
+    return $copy
   }
 
+  Get-Date -Format "HH:mm:ss.ms" | Add-Content $LogFile
+}
 
-iex "cmd /c echo %time% >> t.out"
+Process {
+  $files = Get-ChildItem -recurse $SourceDir
+  foreach ($file in $files) {
+    # directories
+    if (-not $file.psiscontainer) { 
+      if (checkfile($file.fullname) -eq 1) { 
+        # nothing to do here
+        # maybe add to the logfile if a -v option is added
+      }
+      else { 
+        ("{0} - SUSPECTED RANSOMWARE ENCRYPTED" -f $file.fullname) | Add-Content $LogFile
+        Write-Output ("{0} - SUSPECTED RANSOMWARE ENCRYPTED" -f $file.fullname)
+      }
+    }
+  }
+}
+
+End {
+  Get-Date -Format "HH:mm:ss.ms" | Add-Content $LogFile
+}
